00dacea39b06d949f24fc683542dcfcc4acb4ad6
Roman Velnar <roman.velnar@gmail.com>
2022-07-24 14:34:47 +0100

    fresh commit before opening a branch

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,15 +1,18 @@
 import React from "react";
 import tw from "twin.macro";
+import Facilities from "./Facilities"
 
 const Container = tw.div`px-10 pt-10`
-const Heading = tw.h1`text-xl text-primary-600`
+
+const Heading = tw.h1`text-3xl font-bold text-primary-600 text-center`
 
 
 const Services = () => {
 
     return(
         <Container>
-            <Heading>Hello World</Heading>
+            <Heading>Facilities</Heading>
+            <Facilities />
         </Container>
     )
 }
