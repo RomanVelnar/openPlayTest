ded94f672f405903f2b1f9cd5c18e243e7cbaea5
Roman Velnar <roman.velnar@gmail.com>
2022-07-27 21:12:40 +0100

    created Activities.js and wrote pseudo code to connect the the two data sets and display activites

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './assets/css/App.css';
 import Home from './pages/home/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" />
+        </Routes>
+      </div>
+    </Router>
     );
 }
 
--- src/pages/services/Facilities.js
+++ src/pages/services/Facilities.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import tw from "twin.macro";
 import facilities from "../../data/facilities.json";
+import activities from "../../data/activities.json"
 
 const Container = tw.div`grid grid-cols-3`
 const FacilitiesCard = tw.div`w-1/3 p-4 rounded cursor-pointer bg-primary-500 hover:bg-primary-300`
@@ -9,6 +11,7 @@ const Heading = tw.h2`text-center`
 const Facilities = ({ facilitiesResult }) => {
 
     let locations = structuredClone(facilities.data);
+    let activitiesData = structuredClone(activities.data)
     console.log(locations)
     
     return(
@@ -31,6 +34,7 @@ const Facilities = ({ facilitiesResult }) => {
                     .map((facility, value) => (
                         <FacilitiesCard key={value}>
                             <Heading>{facility.name.replaceAll('_', ' ')}</Heading>
+                            <Link to={`/activities/${locations.id}`}>View</Link>
                         </FacilitiesCard>
                 ))}
 
