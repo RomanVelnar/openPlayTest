fba3f709ac1a57aaf43a59959ff4078719c15037
Roman Velnar <roman.velnar@gmail.com>
2022-08-02 14:57:15 +0100

    further refactoring

--- src/pages/activities/Activity.js
+++ src/pages/activities/Activity.js
@@ -1,22 +1,14 @@
 import React from "react";
 import tw from "twin.macro";
-import activities from "../../data/activities.json";
 import ActivitiesCard from "../../components/Card/ActivitiesCard";
 
 const Container = tw.div`grid grid-cols-3 gap-10`
 
-const Activity = ({activitiesResult, props}) => {
-
-    // const [data, setData] = useState([]);
-
-    let activitiesData = structuredClone(activities.data);
-
-    console.log(activitiesData)
+const Activity = ({activitiesResult}) => {
 
     return(
         <Container>
             {activitiesResult
-                // .sort((a, b) => a.start_time - b.start_time)
                 .sort((a, b) => parseFloat(a.start_time) - parseFloat(b.start_time))
                 .map((activity, value) => (
                     <ActivitiesCard 
